a554955ca937d3f3cd6b92e911e1cd00d8cc5466
Lionhatt <gavindxy@gmail.com>
2021-04-18 19:44:11 +1000

    create function to generate radom data in csv

---
+++ createCsv.py
@@ -0,0 +1,35 @@
+import csv
+import random
+from time import time
+from faker import Faker
+
+RECORD_COUNT = 500000
+fake = Faker()
+
+
+
+def create_csv_file():
+    with open('Dask-Test/files/test.csv', 'w', newline='') as csvfile:
+        fieldnames = ['user_id', 'isAuthenticated', 'time']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+        for i in range(RECORD_COUNT):
+            writer.writerow(
+                {
+                    'user_id': fake.random_int(min=1, max=100000),
+                    'isAuthenticated': fake.boolean(chance_of_getting_true=50),
+                    'time': fake.date_time_this_decade(),
+                }
+            )
+
+if __name__ == '__main__':
+    start = time()
+    create_csv_file()
+    elapsed = time() - start
+    print('created csv file time: {}'.format(elapsed))
+
+
+
+
+
